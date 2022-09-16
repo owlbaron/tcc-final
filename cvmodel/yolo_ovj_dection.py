eb1e72cd7797b4ecb529f0010296cee2e476386b
Igor Miyamoto <igor.kenzo.m@gmail.com>
2022-09-16 19:40:27 -0300

    carregando o modelo

---
+++ cvmodel/yolo_ovj_dection.py
@@ -0,0 +1,153 @@
+from ast import Str
+import cv2
+import numpy as np
+import glob
+import random
+import shutil
+
+
+class DarknetModel():
+    """ Responsável por carregar o modelo a partir do arquivo de configuração e os pesos. 
+    :param cfg_path : caminho para o arquivo .cfg do modelo
+    :param weights_path : caminho para o arquivo .weights do modelo
+    :param classes_name : o nome das classes. Precisa estar na mesma ordem que a do treinamento
+    """
+    def __init__(self, cfg_path : Str, weights_path: Str, classes_name: list[Str]) -> None:
+        # Load YoloPYT
+        self.net = cv2.dnn.readNetFromDarknet(cfg_path, weights_path)
+        self.classes = classes_name
+        self.layer_names = self.net.getLayerNames()
+        self.output_layers = [self.layer_names[i-1] for i in self.net.getUnconnectedOutLayers()]
+        
+    def detect(self, img_path):
+        # Loading image
+        img = cv2.imread(img_path)
+        # img = cv2.resize(img, None, fx=0.4, fy=0.4)
+        height, width, channels = img.shape
+
+        # Detecting objects
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+        self.net.setInput(blob)
+        outs = self.net.forward(self.output_layers)
+
+        class_ids = []
+        confidences = []
+        boxes = []
+        labeldata = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.3:
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    labeldata.append([class_id, detection[0], detection[1], detection[2], detection[3]])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        for conf in confidences:
+            print(f"class_id: {class_id}; classe: {self.classes[class_id]}; confiança: {conf}")
+
+
+# # Load YoloPYT
+# net = cv2.dnn.readNetFromDarknet("/home/miyamoto/projects/tcc/darknet/nosso-yolo/yolov4-obj.cfg", "/home/miyamoto/projects/tcc/darknet/nosso-yolo/data/backup/yolov4-obj_best.weights")
+
+# # Name custom object
+# classes = ["battle", "exploration", "menu"]
+
+# # Images path
+# images_path = glob.glob(r"/home/miyamoto/projects/tcc/darknet/nosso-yolo/data/validation/*.jpg")
+
+# layer_names = net.getLayerNames()
+# output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
+# colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# # Insert here the path of your images
+# random.shuffle(images_path)
+# print(len(images_path))
+# # loop through all the images
+# for img_num, img_path in enumerate(images_path):
+#     # Loading image
+#     img = cv2.imread(img_path)
+#     # img = cv2.resize(img, None, fx=0.4, fy=0.4)
+#     height, width, channels = img.shape
+
+#     # Detecting objects
+#     blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+#     net.setInput(blob)
+#     outs = net.forward(output_layers)
+
+#     # Showing informations on the screen
+#     class_ids = []
+#     confidences = []
+#     boxes = []
+#     labeldata = []
+#     for out in outs:
+#         for detection in out:
+#             scores = detection[5:]
+#             class_id = np.argmax(scores)
+#             confidence = scores[class_id]
+#             if confidence > 0.3:
+#                 # Object detected
+#                 center_x = int(detection[0] * width)
+#                 center_y = int(detection[1] * height)
+#                 w = int(detection[2] * width)
+#                 h = int(detection[3] * height)
+
+#                 # Rectangle coordinates
+#                 x = int(center_x - w / 2)
+#                 y = int(center_y - h / 2)
+
+#                 boxes.append([x, y, w, h])
+#                 labeldata.append([class_id, detection[0], detection[1], detection[2], detection[3]])
+#                 confidences.append(float(confidence))
+#                 class_ids.append(class_id)
+                
+#     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+#     font = cv2.FONT_HERSHEY_PLAIN
+#     has = False
+#     for i in range(len(boxes)):
+#         if i in indexes:
+#             x, y, w, h = boxes[i]
+#             label = str(classes[class_ids[i]])
+#             color = colors[class_ids[i]]
+#             cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+#             cv2.putText(img, label, (x, y + 30), font, 1, color, 1)
+#             has = True
+
+#     if has:
+#         cv2.imshow("Image", img)
+#         key = cv2.waitKey(0)
+
+#     #     savepath = "./results"
+
+#     #     if key == 113:
+#     #         exit()
+#     #     elif key == 27:
+#     #         continue
+#     #     elif key == 32:
+#     #         shutil.copyfile(img_path, savepath + "\\ajustar\\" + img_path.split("\\")[-1])
+#     #     else:
+#     #         shutil.copyfile(img_path, savepath + img_path.split("\\")[-1])
+#     #         txtFile = img_path.split("\\")[-1].split(".")[0] + ".txt"
+#     #         f = open(savepath + txtFile, 'w')
+#     #         for i in range(len(labeldata)):
+#     #             if i in indexes:
+#     #                 f.write(str(labeldata[i][0]) + " " + str(labeldata[i][1]) + " " + str(labeldata[i][2]) + " " + str(labeldata[i][3]) + " " + str(labeldata[i][4]) + "\n")
+#     #         f.close()
+
+#     print(str(img_num) + "/" + str(len(images_path)))
+
+# cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from asr.gcloud import GCloud
 from asr.lvosk import Vosk
 from microphone.microphone import MicrophoneStream
 import os
+from cvmodel.yolo_ovj_dection import DarknetModel
 
 RATE = 16000
 CHUNK = int(RATE / 10)
@@ -33,28 +34,51 @@ def test():
     import pywinctl as pwc
     import subprocess
     # import json
-    from pywinctl import Win32Window
     import pyautogui
 
-    subprocess.Popen('notepad')
-    sleep(0.5)
-    windows = pwc.getWindowsWithTitle('bloco de notas', condition=pwc.Re.CONTAINS, flags=pwc.Re.IGNORECASE)
+    subprocess.Popen('mgba-qt')
+    sleep(1)
+    windows = pwc.getWindowsWithTitle('mgba', condition=pwc.Re.CONTAINS, flags=pwc.Re.IGNORECASE)
     # print(pwc.getAllTitles())
     # print(windows)
     if windows:
-        win : Win32Window = windows[0]
+        win : pwc.Window = windows[0]
         # menu = win.menu.getMenu()
         # print(json.dumps(menu, indent=4, ensure_ascii=False))  # Prints menu dict in legible format
         # ret = win.menu.clickMenuItem(["File", "Exit"])           # Exit program
         # if not ret:
             # print("Option not found. Check option path and language")
 
-        im = pyautogui.screenshot("banana.png", region=win.box)
+        im = pyautogui.screenshot(None, region=win.box)
         print(win.topright, win.topleft, win.top, win.width, win.height, win.box)
         print(im)
     else:
         print("Window not found. Check application name and language")
 
+def model():
+    darknet_model = DarknetModel(
+        cfg_path= "/home/miyamoto/projects/tcc/darknet/nosso-yolo/yolov4-obj.cfg", 
+        weights_path= "/home/miyamoto/projects/tcc/darknet/nosso-yolo/data/backup/yolov4-obj_best.weights",
+        classes_name= ["battle", "exploration", "menu"]
+    )
+
+    darknet_model.detect("/home/miyamoto/projects/tcc/darknet/nosso-yolo/dataset/va/batalha19442_png.rf.973db3164f20398879f2090e2ed3e412.jpg")
+
 if __name__ == "__main__":
     # main()
-    test()
+    # test()
+    model()
+
+#####
+
+# No Manjaro (KDE) tava dando esse erro
+
+# QObject::moveToThread: Current thread (...) is not the object's thread (...).
+# Cannot move to target thread (...)
+# qt.qpa.plugin: Could not load the Qt platform plugin "xcb" in "/(...)site-packages/cv2/qt/plugins" even though it was found.
+# This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem.
+
+
+# jeito que funcionou, foi a ultima resposta do link, aparentemente como o opencv trata alguma coisa
+# instalar o opencv headless
+#https://github.com/NVlabs/instant-ngp/discussions/300
